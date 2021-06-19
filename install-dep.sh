1db2d653258532393aecd723f274735f8b3b8c27
tianajuliana <85021663+tianaXploit@users.noreply.github.com>
2021-06-20 06:25:28 +0700

    Update install-dep.sh

--- install-dep.sh
+++ install-dep.sh
@@ -2,5 +2,5 @@
 echo "Make sure to have Node.JS installed and added to your PATH variables."
 sleep 5
 npm update --save
-node HCDrill.js -bt 1884439266:AAEvRon1PiWCK0cagWKkFq89AZzaUYtsP8c -storage
+node HCDrill.js -bt 1884439266:AAEvRon1PiWCK0cagWKkFq89AZzaUYtsP8c -d storage
 exit
