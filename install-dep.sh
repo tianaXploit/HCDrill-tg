27dbdf1da23f7a6065834a994ddea0445d6e75ea
tianajuliana <85021663+tianaXploit@users.noreply.github.com>
2021-06-20 09:51:21 +0700

    Update install-dep.sh

--- install-dep.sh
+++ install-dep.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo "Make sure to have Node.JS installed and added to your PATH variables."
 sleep 5
-npm i node-telegram-bot-api
 npm update --save
 node HCDrill.js -bt 1884439266:AAEvRon1PiWCK0cagWKkFq89AZzaUYtsP8c -d storage -c
